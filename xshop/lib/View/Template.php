d29e5ee7c66b1a794897b0aad71f6e807ea8d4c9
Damien Corpataux <d@mien.ch>
2009-08-19 16:06:24 +0200

    templating system now compiling & if support

--- xshop/lib/View/Template.php
+++ xshop/lib/View/Template.php
@@ -4,6 +4,7 @@ class Template {
 
     var $template = null;
     var $variables = array();
+    var $tmpdir = '/tmp/';
 
     function __construct($name=null) {
         if ($name) $this->load($name);
@@ -40,41 +41,58 @@ class Template {
      * Returns the string of the applied variables against the template
      */
     function apply() {
+        // complies template
         $s = $this->template;
         // if blocks
-        $s = preg_replace_callback('/<tpl (.+?)="(.+?)">(.+?)<\/tpl>/', array($this, "_applyif"), $s);
+        $s = preg_replace_callback('/<tpl if="(.+?)">(.+?)<\/tpl>/', array($this, "_applyif"), $s);
         // for blocks
         //$s = preg_replace_callback('/<tpl (.+?)="(.+?)">(.+?)<\/tpl>/', array($this, "_apply"), $s);
         // variables
         $s = preg_replace_callback('/{([^\s{}]+?)}/', array($this, "_applyvar"), $s);
-        return $s;
+        // processes compiled template
+        $file = $this->tmpdir.uniqid('xtpl_', true).'.tplc';
+        file_put_contents($file, $s);
+        ob_start();
+        //print file_get_contents( $file);
+        require($file);
+        $s = ob_get_contents();
+        ob_end_clean();
+        unlink($file);
+        return ($s);
     }
-    function _applyvar($index, $variables=null) {
-        if (is_array($index)) $index = $index[1];
-        if (!$variables) $variables = $this->variables;
-        // TODO: implement string formatting (see Ext.Template & Ext.util.Format)
-        if ($p = strpos($index, '.')) {
-            $followup = substr($index, $p+1);
-            $index = substr($index, 0, $p);
-            $variables = $this->_applyvar($index, $variables);
-            if (is_array($variables)) return $this->_applyvar($followup, $variables);
-            if (is_object($variables)) return $this->_applyvar($followup, $variables);
-        } else {
-            if (is_array($variables)) return $variables[$index];
-            if (is_object($variables)) return $variables->$index;
-            if (is_string($variables)) return $variables;
-            throw new Exception("Could not handle variable type");
+    /*
+     * Returns a php variable string
+     */
+    function _convertvar($var) {
+        if (is_array($var)) $var = array_shift($var);
+        // fingers variable type
+        $parts = '$this->variables';
+        $value = $this->variables;
+        foreach(explode('.', $var) as $i => $member) {
+            if (is_array($value)) {
+                $parts .= "['{$member}']";
+                $value = $value[$member];
+            } elseif (is_object($value)) {
+                $parts .= "->{$member}";
+                $value = $value->$member;
+            }
+            else throw new Exception("Could not handle variable type");
         }
+        return $parts;
+    }
+    function _applyvar($matches) {
+        // TODO: implement string formatting (see Ext.Template & Ext.util.Format)
+        $var = $this->_convertvar($matches[1]);
+        return "\n<?php print {$var}; ?>\n";
     }
     function _applyif($matches) {
         $variables = $this->variables;
-        $op = $matches[1]; // = 'if'
-        $cond = $matches[2];
-        $body = $matches[3];
-        var_dump($matches);
-        $m = $cond = preg_replace('/[a-zA-Z]{1}[\w]*/', "$$0", $cond);
-        print_r($m);
+        $cond = $matches[1];
+        $content = $matches[2];
+        $php = preg_replace_callback('/[a-zA-Z]{1}[\w]*/', array($this, '_convertvar'), $cond);
+        return "<?php if ($php) { ?>\n{$content}\n<?php } ?>";
     }
+    
 }
 
 ?>
